a96e70bf0e32d555a40d7c0359e6ac9b00352194
dvankevich <dvankevich@gmail.com>
2024-10-05 20:32:02 +0300

    event handler

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,10 @@
 // src/App.jsx
+import { useState } from 'react';
 import Product from './Product';
 import BookList from './BookList';
 import { Alert } from "./Alert";
 import UserMenu from './UserMenu';
+
 
 const favouriteBooks = [
   { id: "id-1", name: "JS for beginners" },
@@ -11,9 +13,55 @@ const favouriteBooks = [
   { id: "id-4", name: "React for beginners" }
 ];
 
+const CustomButton = ({ message, children }) => {
+  return (
+    <button onClick={() => alert(message)}>
+      {children}
+    </button>
+  );
+};
+
 export default function App() {
+  const handleClick = (evt) => {
+    console.log(evt);
+    alert("I'm a button! See console log.");
+  };
+  
+  // let clicks = 0;
+  const [ clicks, setClicks ] = useState(10);
+
+  const handleClickR = () => {
+    //clicks = clicks + 1;
+    setClicks(clicks + 1);
+  };
+
   return (
     <div>
+      <div>
+        <h2>reactive</h2>
+
+        <button onClick={handleClickR}>Current: {clicks}</button>
+
+      </div>
+      <div>
+        <h2>reading props</h2>
+        <CustomButton message="Playing music!">
+        Play some music
+        </CustomButton>
+        <CustomButton message="Uploading your data!">
+        Upload data
+        </CustomButton>
+      </div>
+      <div>
+        <h2>Button event handler</h2>
+        
+        <button onClick={handleClick}>Click me!</button>
+
+        <button onClick={() => alert("I'm a button. Anonymous inline function!")}>Click me!</button>
+
+        <button onClick={evt => console.log(evt)}>Second button</button>
+      </div>
+
       <h2>User icon</h2>
       <UserMenu name="User Name" />
       
